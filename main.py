3264162190c33911381075b1097f6642b9b4f3ab
Maikiby <maikiby@pm.me>
2025-09-28 19:11:55 -0500

    created delta time

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from constants import *
 
 def main():
     _ = pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("#000000", None, 0)
+        _ = screen.fill("#000000", None, 0)
 
         pygame.display.flip()
 
+        dt = game_clock.tick(60) / 1000
+    
 if __name__ == "__main__":
     main()
